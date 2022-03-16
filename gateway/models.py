a527e22f1a5dac1109a4c52f20a3eb85d369af20
Joseph Chinedu <josephchinedu34@gmail.com>
2022-03-16 15:03:19 +0100

    updated manual repayment logic

--- gateway/models.py
+++ gateway/models.py
@@ -6,8 +6,10 @@ from rest_framework.response import Response
 
 # Create your models here.
 
+
 class Jwt(models.Model):
-    user = models.OneToOneField(CustomUser, related_name="login_user", on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        CustomUser, related_name="login_user", on_delete=models.CASCADE)
     access = models.TextField()
     refresh = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,54 +18,57 @@ class Jwt(models.Model):
 
 class LoanRepayment(models.Model):
 
-    choice=(
+    choice = (
 
-        ('TRANSFER','Transfer'),
-        ('BANK DEPOSIT','Bank Deposit'),
+        ('TRANSFER', 'Transfer'),
+        ('BANK DEPOSIT', 'Bank Deposit'),
         ('CASH', 'Cash'),
         ('CHEQUE', 'Cheque'),
         ('OTHER', 'Other')
 
     )
 
-    user                = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True )
-    phone               = models.CharField(max_length=20, null=True, blank=True)
-    amount              = models.CharField(max_length=50)
-    remita_mandate_id   = models.CharField(max_length=50, null=True, blank=True)
-    payment_date        = models.DateTimeField()
-    entry_date          = models.DateTimeField(auto_now_add=True)
-    updated_at          = models.DateTimeField(auto_now=True)
-    is_approved         = models.BooleanField(default=False)
-    is_flagged          = models.BooleanField(default=False)
-    payment_method      = models.CharField(max_length=30, choices=choice, default=choice[0][0])
-    is_mandate_closed   = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    amount = models.FloatField(default=0.0)
+    remita_mandate_id = models.CharField(max_length=50, null=True, blank=True)
+    payment_date = models.DateTimeField()
+    entry_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_approved = models.BooleanField(default=False)
+    is_flagged = models.BooleanField(default=False)
+    payment_method = models.CharField(
+        max_length=30, choices=choice, default=choice[0][0])
+    is_mandate_closed = models.BooleanField(default=False)
+    is_duplicate = models.BooleanField(default=False)
 
-
-    
     def __str__(self):
         return self.phone
 
     def flag_repayment(payment_date, amount, remita_mandate_id):
-        
-        payment = LoanRepayment.objects.filter(payment_date=payment_date, amount=amount, remita_mandate_id=remita_mandate_id)
-        
+
+        payment = LoanRepayment.objects.filter(
+            payment_date=payment_date, amount=amount, remita_mandate_id=remita_mandate_id)
+
         print(f'Loan Repayment: {payment}')
         if payment.exists():
-            is_flagged = "True"
+            is_flagged = True
             print(f'is flagged value passed: {is_flagged}')
-            result = payment.update(is_flagged = is_flagged)
+            result = payment.update(is_flagged=is_flagged)
             print(f'update query result: {result}')
             try:
                 result < 1
             except:
-                result = payment.update(is_flagged = is_flagged)
+                result = payment.update(is_flagged=is_flagged)
             else:
-                response = ({'message':'This repayment exist already would you like to proceed'})
+                response = (
+                    {'message': 'This repayment exist already would you like to proceed'})
         return response
 
+
 class Merge(models.Model):
-    payment_id              = models.CharField(max_length=225)
-    email                   = models.EmailField(unique=True)
-    is_staff                = models.BooleanField()
-    is_approved             = models.BooleanField()
-    is_approved_all         = models.BooleanField()
+    payment_id = models.CharField(max_length=225)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField()
+    is_approved = models.BooleanField()
+    is_approved_all = models.BooleanField()
--- gateway/serializers.py
+++ gateway/serializers.py
@@ -1,3 +1,4 @@
+from xml.parsers.expat import model
 from rest_framework.response import Response
 from gateway.models import LoanRepayment, Merge
 from rest_framework import serializers
@@ -8,32 +9,41 @@ class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
+
 class RegisterSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
     name = serializers.CharField()
-   
+
+
 class ResfreshSerializer(serializers.Serializer):
     refresh = serializers.CharField()
+
 
 class RepaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = LoanRepayment
-        fields = ["user","id", "phone", "amount","remita_mandate_id", "payment_date","payment_method"]
+        fields = ["user", "id", "phone", "amount",
+                  "remita_mandate_id", "payment_date", "payment_method"]
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    
+
     old_password = serializers.CharField()
     new_password = serializers.CharField()
+
 
 class ApproveoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Merge
-        fields = ["payment_id", "email", "is_staff","is_approved"]
+        fields = ["payment_id", "email", "is_staff", "is_approved"]
 
 
 class ApproveallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Merge
-        fields = ["email", "is_staff","is_approved_all"] 
+        fields = ["email", "is_staff", "is_approved_all"]
+
+
+class ConfirmRepaymentSerializer(serializers.Serializer):
+    phone = serializers.CharField()
