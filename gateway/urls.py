2973b180b961f11157e5eb5d8792eb0e02097e94
Lloyds01 <soloyede708@gmail.com>
2022-03-25 04:59:26 -0700

    t

--- gateway/urls.py
+++ gateway/urls.py
@@ -2,7 +2,7 @@
 from multiprocessing.managers import Namespace
 from xml.etree.ElementInclude import include
 from django.urls import path
-from .views import *
+from gateway.views import *
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
